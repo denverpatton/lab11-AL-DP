6d1ebefa3146122983b886818694d2e93c884809
denverpatton <denverpatton52@gmail.com>
2025-11-16 17:47:41 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -23,7 +23,7 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -35,7 +35,7 @@ def div(a, b):
         raise ZeroDivisionError("Cannot divide by zero")
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or b <= 0:
         raise ValueError("Logarithm undefined for non-positive inputs.")
     if a == 1:
